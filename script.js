14ea4bf7e5ef9b7e2bbc9db9f7340fe232ac38d0
sophiabotero-eng <sophiabotero@gmail.com>
2026-04-17 14:07:43 -0400

    Add functions for scrolling and alerts in script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollToTutorials() {
+  document.getElementById("tutorials").scrollIntoView();
+}
+
+function watchFree() {
+  alert("Play your free tutorial here");
+}
+
+function unlock() {
+  window.location.href = "https://buy.stripe.com/test"; // replace later
+}
